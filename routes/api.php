acd9ae7e7e28101442ae23109caef48756a5be7b
joan <oob071947@gmail.com>
2026-03-05 22:17:17 -0600

    Creación Usercontroller

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//crear user
+Route::post('/users', [UserController::class, 'create']);
