b4018f4ae4545e9cc4cf7fd5a7c0aa31b55ed9fe
Emiliano Pacheco Berlanga <emilianopachecoberlanga@gmail.com>
2026-03-06 00:53:05 -0600

    AuthApiController

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiAuthController;
+use App\Http\Controllers\AuthApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
