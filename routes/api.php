bd7fa2ebce510e9d8da5987708a8175bc8263a95
Emiliano Pacheco Berlanga <emilianopachecoberlanga@gmail.com>
2026-03-06 00:40:05 -0600

    Api Rutas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiAuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//LOGIN AND REGISTER
+Route::post('/register', [ApiAuthController::class, 'register']);
+Route::post('/login', [ApiAuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [ApiAuthController::class, 'logout']);
