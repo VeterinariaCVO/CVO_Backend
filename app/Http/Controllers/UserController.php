aeafc5b43be3d3691f9a1e56a33a31349c0d47e5
julioatracks1@gmail.com <julioo87171@gmail.com>
2026-03-09 10:18:04 -0600

    descargar cambios desarrollo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -12,8 +13,8 @@ class UserController extends Controller
      * Display a listing of the resource.
      */
 
-    
-        public function create(Request $request){
+
+    public function create(Request $request){
     $data = $request->validate([
         'name' => 'required|string|max:255',
         'email' => 'required|string|email|max:255',
@@ -34,11 +35,33 @@ class UserController extends Controller
     ],201);
 
     }
-    public function index()
+    public function index(Request $request)
     {
-        //
+    $query = DB::table('users')
+        ->join('roles', 'users.role_id', '=', 'roles.id')
+        ->select(
+            'users.id',
+            'users.name',
+            'users.email',
+            'users.phone',
+            'users.address',
+            'users.active',
+            DB::raw('roles.description as role'),
+            DB::raw("CASE
+            WHEN users.active = 1 THEN 'Active'
+            ELSE 'Inactivo'
+            END as status")
+        )
+        ->where('roles.description', 'client');
+
+    if ($request->search) {
+        $query->where('users.name', 'like', '%' . $request->search . '%');
     }
 
+    $clients = $query->get();
+
+    return response()->json($clients);
+    }
     /**
      * Store a newly created resource in storage.
      */
@@ -60,14 +83,30 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+    $data = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255',
+        'phone' => 'nullable|string|max:20',
+        'address' => 'nullable|string|max:255',
+        'active' => 'required|boolean',
+    ]);
+
+    $user = User::findOrFail($id);
+    $user->update($data);
+
+    return response()->json([
+        'message' => 'Cliente actualizado correctamente',
+        'user' => $user
+    ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+    $user = User::findOrFail($id);
+    $user->delete();
+
+    return response()->json([
+        'message' => 'Cliente eliminado correctamente'
+    ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiAuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,8 +11,14 @@ Route::get('/user', function (Request $request) {
 
 //crear user
 Route::post('/users', [UserController::class, 'create']);
+//listasUsaurio
+Route::get('clients',[UserController::class,'index']);
+Route::put('/clients/{id}', [UserController::class, 'update']);
+Route::delete('/clients/{id}', [UserController::class, 'destroy']);
 
 //LOGIN AND REGISTER
 Route::post('/register', [ApiAuthController::class, 'register']);
 Route::post('/login', [ApiAuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [ApiAuthController::class, 'logout']);
+
+
